1c25f2026669c49e9c61881b99bedeccb28d8f9c
Andrey <asindeev@open-sys.org>
2019-03-26 14:20:18 +0400

    Conditional operator

--- src/main/java/FirstLesson.java
+++ src/main/java/FirstLesson.java
@@ -2,9 +2,27 @@ public class FirstLesson {
 
     public static void main(String[] args) {
 
-        String a = "Hello";
-        String b = "World";
-        System.out.println(a + " " + b + "!");
+        /*
+            Объявляем переменные a,b
+            Переменной а будет присвоено значение 10
+            Переменной b , будет присвоено выражение
+            Если выражение заключенное в скобки будет возвращать true, тогда ответ будет 20
+            Если выражение заключенное в скобки будет возвращать false, тогда ответ будет 30
+
+         */
+        //False example
+        int a, b;
+        a = 10;
+        b = (a == 1) ? 20: 30;
+        System.out.println("Value of b is: " + b);
+        //True example
+        int c, d;
+        c = 10;
+        d = (c == 10) ? 20: 30;
+        System.out.println("Value of d is: " + d);
+
+
+
 
     }
 
