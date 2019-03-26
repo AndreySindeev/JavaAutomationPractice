bb8dc57adc4afd2fd54167d5e71f7c3ca682659c
Andrey <asindeev@open-sys.org>
2019-03-26 14:53:41 +0400

    Finish ferst lesson

--- src/main/java/FirstLesson.java
+++ src/main/java/FirstLesson.java
@@ -3,6 +3,7 @@ public class FirstLesson {
     public static void main(String[] args) {
 
         /*
+            Тернарный оператор
             Объявляем переменные a,b
             Переменной а будет присвоено значение 10
             Переменной b , будет присвоено выражение
@@ -21,11 +22,22 @@ public class FirstLesson {
         d = (c == 10) ? 20: 30;
         System.out.println("Value of d is: " + d);
 
+        int i;
+        for (i = 0; i < 20; i++){
+            System.out.println("Value i is: "+ i);
+        }
 
+        int salaries[] = {1244, 3423, 4343, 3434};
+        for(i = 0; i <salaries.length; i++) {
+            System.out.println(salaries[i]);
+        }
+        String gender = "Мужчина";
+        System.out.println((gender.equals("Женщина")? "Мисс" : "Господин"));
 
+        }
 
     }
 
 
 
-}
+
